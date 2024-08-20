be095207c4a85e70cf48e442f67d7a4c2d242b4f
Anton Volnuhin <anton@volnuhin.com>
2024-08-20 07:06:35 +0300

    fix a bug that wouldn't let script filter data

--- generate.fish
+++ generate.fish
@@ -20,6 +20,7 @@ end
 
 function generate-ips
 	set name $argv[1]
+	set searchline $argv[2]
 	echo
 	echo    "### Generating lists for $name"
 	echo -n "    Filtering ipv6s.."
@@ -35,7 +36,7 @@ function generate-ips
 	echo " Merging ipv4s.."
 	python utils/merge.py --source $name.ipv4.cidr >$name.ipv4.cidr.txt
 
-    rm *.cidr
+	#rm *.cidr
 end
 
 # Download data and filter it by ip protocol version
